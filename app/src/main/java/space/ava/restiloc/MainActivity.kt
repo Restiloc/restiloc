11986c394e57e59ec779b6ac2cff8c401e55034d
dinholu <alizeehett@hotmail.fr>
2023-01-21 17:21:12 +0100

    add layout

--- app/src/main/java/space/ava/restiloc/MainActivity.kt
+++ app/src/main/java/space/ava/restiloc/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_planning, R.id.navigation_stat, R.id.navigation_setting
+                R.id.navigation_planning, R.id.navigation_statistiques, R.id.navigation_settings
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
