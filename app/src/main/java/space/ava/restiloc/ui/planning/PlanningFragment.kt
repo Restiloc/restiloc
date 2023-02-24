48f56f14d6890cb27db8dbadce64183f493e09ee
dinholu <alizeehett@hotmail.fr>
2023-02-24 12:07:45 +0100

    add api call

--- app/src/main/java/space/ava/restiloc/ui/planning/PlanningFragment.kt
+++ app/src/main/java/space/ava/restiloc/ui/planning/PlanningFragment.kt
@@ -1,12 +1,18 @@
 package space.ava.restiloc.ui.planning
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import space.ava.restiloc.ApiInterface
 import space.ava.restiloc.R
 import space.ava.restiloc.classes.*
 import space.ava.restiloc.databinding.FragmentPlanningBinding
@@ -31,67 +37,38 @@ class PlanningFragment(
         _binding = FragmentPlanningBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-       // val textView: TextView = binding.textPlanning
-       // planningViewModel.text.observe(viewLifecycleOwner) {
-       //     textView.text = it
-       // }
-
-        // créer une liste qui va stocker ces elements
-        val planningList = arrayListOf<Mission>()
-
-        // enregistrer une premiere voiture
 
 
-        val expert1 = Expert(
-            3, "Jean", "Valjean", "PetitJean", "petitjean@hotmail.fr", "061815778", "https://restiloc.fr"
-        )
+        // recuperer les données de l'API
 
-        val garage1 = Garage(
-            1, "LeGarage", "6 ", "Strasbourg", "155512", "5456456", "0618484848", "67522", "rue des pieds", "https://restiloc.fr"
-        )
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://restiloc.space/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
-        val preeList = arrayListOf<Pree>()
-        preeList.add(Pree(
-            1, "Je sais pas ce que c'est", "C'est une super description", "https://restiloc.space", "https://restiloc.space", "Alizée"
-        )
-        )
-        preeList.add(Pree(
-            2, "Je sais pas ce que c'est", "C'est une super description", "https://restiloc.space", "https://restiloc.space", "Alizée"
-        )
-        )
+        val apiService = retrofit.create(ApiInterface::class.java)
+        val planningList = ArrayList<Mission>()
 
-        val unavailability1 = Unavailability(
-            1, 2, "restiloc.space"
-        )
+        // appel asynchrone de l'API
+        lifecycleScope.launch {
+            try {
+                val missions = apiService.getInfos()
+                for (mission in missions) {
+                    planningList.add(mission)
+                }
+                val verticalRecyclerView = root?.findViewById<RecyclerView>(R.id.vertical_recycler_view)
+                verticalRecyclerView?.adapter = MeetingAdapter(planningList, R.layout.item_horizontal)
+                verticalRecyclerView?.addItemDecoration(MeetingItemDecoration())
+                Log.d("test", "test")
 
-        val vehicle1 = Vehicle(
-            1, "rouge", "AHDUDS", 2021, "https://restiloc.space"
-        )
+            } catch (e: Exception) {
+                // PrintStackTrace pour afficher l'erreur
+                e.printStackTrace()
+                // verifie dans quel condition je suis
+                Log.d("test", "testError")
+            }
+        }
 
-        planningList.add(
-            Mission(
-                1, "10-12-23", expert1, garage1, true, 15500,"XLFEHDS", preeList, "https://restiloc.space", "10-02-23", unavailability1 , vehicle1)
-        )
-
-        planningList.add(
-            Mission(
-                2, "10-12-23", expert1, garage1, true, 15500,"XLFEHDS", preeList, "https://restiloc.space", "10-02-23", unavailability1 , vehicle1)
-        )
-
-        planningList.add(
-            Mission(
-                3, "10-12-23", expert1, garage1, true, 15500,"XLFEHDS", preeList, "https://restiloc.space", "10-02-23", unavailability1 , vehicle1)
-        )
-
-        planningList.add(
-            Mission(
-                4, "10-12-23", expert1, garage1, true, 15500,"XLFEHDS", preeList, "https://restiloc.space", "10-02-23", unavailability1 , vehicle1)
-        )
-
-
-        val verticalRecyclerView = root?.findViewById<RecyclerView>(R.id.vertical_recycler_view)
-        verticalRecyclerView?.adapter = MeetingAdapter(planningList, R.layout.item_horizontal)
-        verticalRecyclerView?.addItemDecoration(MeetingItemDecoration())
         return root
     }
 
