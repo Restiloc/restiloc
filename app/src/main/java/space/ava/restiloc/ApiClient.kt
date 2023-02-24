f92fd4ac355acf5e1b6e9e677deba5b43213ff65
dinholu <alizeehett@hotmail.fr>
2023-02-24 12:04:14 +0100

    add api client

---
+++ app/src/main/java/space/ava/restiloc/ApiClient.kt
@@ -0,0 +1,20 @@
+package space.ava.restiloc
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+
+object ApiClient {
+    private const val BASE_URL = "https://restiloc.ava.space/"
+
+    private val gson : Gson = GsonBuilder()
+        .setLenient()
+        .create()
+
+    private val retrofit = retrofit2.Retrofit.Builder()
+        .baseUrl(BASE_URL)
+            // convertir en objet json
+        .addConverterFactory(retrofit2.converter.gson.GsonConverterFactory.create(gson))
+        .build()
+
+    val apiService = retrofit.create(ApiInterface::class.java)
+}
