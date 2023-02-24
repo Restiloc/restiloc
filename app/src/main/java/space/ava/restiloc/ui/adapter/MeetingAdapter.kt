0faca5f5ecd57d8e23e4c3edd0dbbf54c61ac74d
dinholu <alizeehett@hotmail.fr>
2023-02-24 12:06:33 +0100

    fix MeetingAdapter.kt

--- app/src/main/java/space/ava/restiloc/ui/adapter/MeetingAdapter.kt
+++ app/src/main/java/space/ava/restiloc/ui/adapter/MeetingAdapter.kt
@@ -6,20 +6,20 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import space.ava.restiloc.MainActivity
+import com.bumptech.glide.Glide
 import space.ava.restiloc.R
 import space.ava.restiloc.classes.Mission
-import space.ava.restiloc.classes.Vehicle
+
 
 class MeetingAdapter (
     private val planningList : List<Mission>,
     private val layoutId: Int) : RecyclerView.Adapter<MeetingAdapter.ViewHolder>()
 {
-    //Boite pour ranger tous les composants à controler
+    // Boite pour ranger tous les composants à controler
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        val MeetingImage = view.findViewById<ImageView>(R.id.image_item)
-        val MeetingName:TextView? = view.findViewById(R.id.name_item)
-        val MeetingDescription:TextView? = view.findViewById(R.id.description_item)
+        val meetingImage: ImageView = view.findViewById(R.id.image_item)
+        val meetingName:TextView? = view.findViewById(R.id.name_item)
+        val meetingDescription:TextView? = view.findViewById(R.id.description_item)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -31,12 +31,15 @@ class MeetingAdapter (
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        // Recuperer les informations de la plante
+        // Recuperer les informations du planning
         val currentMeeting = planningList[position]
 
-        // mettre a jour le nom du vehicule
-        holder.MeetingName?.text = currentMeeting.nameExpertFile
-        holder.MeetingDescription?.text = currentMeeting.dateMission
+        // test pour ajouter une image tirée d'internet
+        //Glide.with(holder.meetingImage.context)
+        //    .load(currentMeeting.vehicle.route)
+        //    .into(holder.meetingImage)
+        holder.meetingName?.text = currentMeeting.nameExpertFile
+        holder.meetingDescription?.text = currentMeeting.dateMission
     }
 
     override fun getItemCount(): Int = planningList.size
