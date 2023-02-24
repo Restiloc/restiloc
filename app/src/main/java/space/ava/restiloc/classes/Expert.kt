9b23066547a5ef6a8abcbbe9339ea0eba1416d81
dinholu <alizeehett@hotmail.fr>
2023-02-24 12:05:02 +0100

    repare class Expert

--- app/src/main/java/space/ava/restiloc/classes/Expert.kt
+++ app/src/main/java/space/ava/restiloc/classes/Expert.kt
@@ -4,8 +4,8 @@ data class Expert(
     val id: Int,
     val lastName: String,
     val firstName: String,
-    val username: String,
     val email: String,
     val phoneNumber: String,
+    val username: String,
     val route: String
 )
