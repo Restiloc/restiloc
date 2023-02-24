f0dbd969736a5fb94f5e77e2f582133de6e36e08
dinholu <alizeehett@hotmail.fr>
2023-02-24 12:08:16 +0100

    fix Vehicle class

--- app/src/main/java/space/ava/restiloc/classes/Vehicle.kt
+++ app/src/main/java/space/ava/restiloc/classes/Vehicle.kt
@@ -2,8 +2,8 @@ package space.ava.restiloc.classes
 
 data class Vehicle(
     val id: Int,
-    val color: String,
     val licencePlate: String,
+    val color: String,
     val releaseYear: Int,
     val route: String
 )
