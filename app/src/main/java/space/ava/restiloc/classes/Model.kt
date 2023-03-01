cdae70ce4dad784672d49a8bdbbdfbcc9ffb8535
dinholu <alizeehett@hotmail.fr>
2023-03-01 16:30:19 +0100

    add Model class

---
+++ app/src/main/java/space/ava/restiloc/classes/Model.kt
@@ -0,0 +1,10 @@
+package space.ava.restiloc.classes
+
+class Model (
+    val id: Int,
+    val label : String,
+    val brand: String,
+    val created_at : String,
+    val updated_at : String,
+
+        )
