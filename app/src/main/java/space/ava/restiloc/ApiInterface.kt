cc5f5a2c5bf0551222a30ff17b71f4686316787d
dinholu <alizeehett@hotmail.fr>
2023-03-03 23:38:05 +0100

    add header autorisation

--- app/src/main/java/space/ava/restiloc/ApiInterface.kt
+++ app/src/main/java/space/ava/restiloc/ApiInterface.kt
@@ -1,10 +1,11 @@
 package space.ava.restiloc
 
 import retrofit2.http.GET
+import retrofit2.http.Header
 import space.ava.restiloc.classes.Mission
 
 interface ApiInterface {
-    @GET("api/infos")
+    @GET("api/me/missions")
     // recuperer un objet de type Data
-    suspend fun getInfos(): List<Mission>
+    suspend fun getInfos(@Header("Authorization") token: String): List<Mission>
 }
