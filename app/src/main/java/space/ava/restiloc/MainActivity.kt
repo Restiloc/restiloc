324fa3c6eff451b9fa565a9d806da904389bde20
dinholu <alizeehett@hotmail.fr>
2023-03-08 17:16:24 +0100

    add verification if user is connected or not

--- app/src/main/java/space/ava/restiloc/MainActivity.kt
+++ app/src/main/java/space/ava/restiloc/MainActivity.kt
@@ -1,5 +1,6 @@
 package space.ava.restiloc
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -16,22 +17,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val sessionManager = SessionManager(this)
 
-        val navView: BottomNavigationView = binding.navView
+        // Check if user is logged in
+        if (!sessionManager.isLoggedIn()) {
+            // Navigate to LoginActivity if user is not logged in
+            val intent = Intent(this, LoginActivity::class.java)
+           startActivity(intent)
+           finish()
+          return
+        }
+        else {
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_planning, R.id.navigation_statistiques, R.id.navigation_settings
+            val navView: BottomNavigationView = binding.navView
+
+            val navController = findNavController(R.id.nav_host_fragment_activity_main)
+            // Passing each menu ID as a set of Ids because each
+            // menu should be considered as top level destinations.
+            val appBarConfiguration = AppBarConfiguration(
+                setOf(
+                    R.id.navigation_planning, R.id.navigation_statistiques, R.id.navigation_settings
+                )
             )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
+            setupActionBarWithNavController(navController, appBarConfiguration)
+            navView.setupWithNavController(navController)
+        }
 
     }
 }
