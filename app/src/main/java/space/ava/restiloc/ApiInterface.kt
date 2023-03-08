36d3f9a55054a8cba3cc3225dd7864e6cb4779ce
dinholu <alizeehett@hotmail.fr>
2023-03-08 18:59:35 +0100

    add function to connect

--- app/src/main/java/space/ava/restiloc/ApiInterface.kt
+++ app/src/main/java/space/ava/restiloc/ApiInterface.kt
@@ -1,11 +1,17 @@
 package space.ava.restiloc
 
-import retrofit2.http.GET
-import retrofit2.http.Header
+import retrofit2.Call
+import retrofit2.http.*
+import space.ava.restiloc.classes.LoginRequest
+import space.ava.restiloc.classes.LoginResponse
 import space.ava.restiloc.classes.Mission
 
 interface ApiInterface {
     @GET("api/me/missions")
     // recuperer un objet de type Data
     suspend fun getInfos(@Header("Authorization") token: String): List<Mission>
+
+    @Headers("Content-Type: application/json")
+    @POST("api/auth/login")
+    fun login(@Body request: LoginRequest): Call<LoginResponse>
 }
