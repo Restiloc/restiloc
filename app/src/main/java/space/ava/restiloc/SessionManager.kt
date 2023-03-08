b0e8034fa5bfbac796c441cf8fc468c8dd614d04
dinholu <alizeehett@hotmail.fr>
2023-03-08 19:01:36 +0100

    add save and fetch token

--- app/src/main/java/space/ava/restiloc/SessionManager.kt
+++ app/src/main/java/space/ava/restiloc/SessionManager.kt
@@ -8,6 +8,20 @@ class SessionManager(context: Context) {
         context.getSharedPreferences("MySharedPreferences", Context.MODE_PRIVATE)
     private val editor: SharedPreferences.Editor = sharedPreferences.edit()
 
+    companion object {
+        const val USER_TOKEN = "user_token"
+    }
+
+    // Save user token
+    fun saveAuthToken(token: String) {
+        editor.putString(USER_TOKEN, token)
+        editor.apply()
+    }
+    fun fetchAuthToken(): String? {
+        return sharedPreferences.getString(USER_TOKEN, null)
+    }
+
+
     fun isLoggedIn(): Boolean {
         return sharedPreferences.getBoolean("isLoggedIn", false)
     }
