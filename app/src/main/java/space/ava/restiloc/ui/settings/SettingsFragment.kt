3327d6d51abe167d9f32c5edf84b6421b0a5dbc9
AlxisHenry <alexis.henry150357@gmail.com>
2023-03-31 09:38:06 +0200

    feat(logout): implement logout

--- app/src/main/java/space/ava/restiloc/ui/settings/SettingsFragment.kt
+++ app/src/main/java/space/ava/restiloc/ui/settings/SettingsFragment.kt
@@ -1,14 +1,24 @@
 package space.ava.restiloc.ui.settings
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import space.ava.restiloc.LoginActivity
+import space.ava.restiloc.R
+import space.ava.restiloc.SessionManager
 import space.ava.restiloc.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
+
+    private lateinit var sessionManager: SessionManager
+
     private var _binding: FragmentSettingsBinding? = null
 
     // This property is only valid between onCreateView and
@@ -20,6 +30,9 @@ class SettingsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        sessionManager = SessionManager(requireContext())
+
         val settingsViewModel =
             ViewModelProvider(this)[SettingsViewModel::class.java]
 
@@ -30,6 +43,16 @@ class SettingsFragment : Fragment() {
         settingsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        val logoutButton = root.findViewById<Button>(R.id.logout)
+
+        logoutButton.setOnClickListener{
+            sessionManager.setLogin(false)
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+
         return root
     }
 
