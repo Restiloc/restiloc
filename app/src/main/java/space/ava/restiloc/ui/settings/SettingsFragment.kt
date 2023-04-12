1704fe4117a4cab1f1b6631bcd4926f099d707fb
AlxisHenry <alexis.henry150357@gmail.com>
2023-04-12 13:33:35 +0200

    hotfix: app crash when token is expired

--- app/src/main/java/space/ava/restiloc/ui/settings/SettingsFragment.kt
+++ app/src/main/java/space/ava/restiloc/ui/settings/SettingsFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import retrofit2.Call
@@ -18,7 +17,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 import space.ava.restiloc.*
 import space.ava.restiloc.classes.*
 import space.ava.restiloc.databinding.FragmentSettingsBinding
-
 
 class SettingsFragment : Fragment() {
 
@@ -49,80 +47,86 @@ class SettingsFragment : Fragment() {
             textView.text = it
         }
 
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://restiloc.space")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
-
-        val apiService = ApiClient.apiService
+        val apiService = retrofit.create(ApiInterface::class.java)
         var expert: Expert = Expert(0, "", "", "", "", "", "");
 
-        apiService.getCurrentExpert("Bearer ${sessionManager.fetchAuthToken()}").enqueue(object : Callback<Expert> {
-            override fun onResponse(call: Call<Expert>, response: Response<Expert>) {
-                val response = response.body()
-                Log.d("Profile", response.toString())
-                expert = response!!
-                val name = root.findViewById<TextView>(R.id.settings_name)
-                val firstname = root.findViewById<TextView>(R.id.settings_firstname)
-                val email = root.findViewById<TextView>(R.id.settings_email)
-                val phone = root.findViewById<TextView>(R.id.settings_tel)
-                name.text = expert.lastName
-                firstname.text = expert.firstName
-                email.text = expert.email
-                phone.text = expert.phoneNumber
-            }
-            override fun onFailure(call: Call<Expert>, t: Throwable) {
-                TODO("Not yet implemented")
-            }
-        })
+        try {
+            apiService.getCurrentExpert("Bearer ${sessionManager.fetchAuthToken()}").enqueue(object : Callback<Expert> {
+                override fun onResponse(call: Call<Expert>, response: Response<Expert>) {
+                    if (response.isSuccessful) {
+                        expert = response.body()!!
+                    }
+                    Log.d("Profile", "Expert : $expert")
+                    root.findViewById<TextView>(R.id.settings_name).text = expert.lastName
+                    root.findViewById<TextView>(R.id.settings_firstname).text = expert.firstName
+                    root.findViewById<TextView>(R.id.settings_email).text = expert.email
+                    root.findViewById<TextView>(R.id.settings_tel).text = expert.phoneNumber
+                }
+                override fun onFailure(call: Call<Expert>, t: Throwable) {
+                    TODO("Not yet implemented")
+                }
+            })
+        } catch (e: Exception) {
+            Log.d("Profile", "Error : ${e.message}")
+        }
 
         val editButton = root.findViewById<Button>(R.id.settings_save)
 
-
-        editButton.setOnClickListener{
-            val updateRequest = UpdateRequest(
-                root.findViewById<TextView>(R.id.settings_name).text.toString(),
-                root.findViewById<TextView>(R.id.settings_firstname).text.toString(),
-                root.findViewById<TextView>(R.id.settings_email).text.toString(),
-                root.findViewById<TextView>(R.id.settings_tel).text.toString()
-            )
-            Log.d("Profile", updateRequest.toString())
-            apiService.updateExpert(
-                token = "Bearer ${sessionManager.fetchAuthToken()}",
-                id = expert.id.toString(),
-                updateRequest = updateRequest
-            ).enqueue(object : Callback<UpdateResponse> {
-                override fun onResponse(call: Call<UpdateResponse>, response: Response<UpdateResponse>) {
-                    val updateResponse = response.body()
-                    Toast.makeText(context, "Vos informations ont bien été mises à jour", Toast.LENGTH_SHORT).show()
-                    Log.d("Profile", "Update response : ${updateResponse.toString()} ${updateRequest.toString()}")
-                }
-                override fun onFailure(call: Call<UpdateResponse>, t: Throwable) {
-                    Toast.makeText(context, "Une erreur est survenue", Toast.LENGTH_SHORT).show()
-                }
-            })
+        try {
+            editButton.setOnClickListener{
+                val updateRequest = UpdateRequest(
+                    root.findViewById<TextView>(R.id.settings_name).text.toString(),
+                    root.findViewById<TextView>(R.id.settings_firstname).text.toString(),
+                    root.findViewById<TextView>(R.id.settings_email).text.toString(),
+                    root.findViewById<TextView>(R.id.settings_tel).text.toString()
+                )
+                Log.d("Profile", expert.id.toString())
+                Log.d("Profile", updateRequest.toString());
+                apiService.updateExpert(
+                    token = "Bearer ${sessionManager.fetchAuthToken()}",
+                    id = expert.id.toString(),
+                    updateRequest = updateRequest
+                ).enqueue(object : Callback<UpdateResponse> {
+                    override fun onResponse(call: Call<UpdateResponse>, response: Response<UpdateResponse>) {
+                        val updateResponse = response.body()
+                        Log.d("Profile", "Update response : ${updateResponse.toString()}")
+                    }
+                    override fun onFailure(call: Call<UpdateResponse>, t: Throwable) {
+                        TODO("Not yet implemented")
+                    }
+                })
+            }
+        } catch (e: Exception) {
+            Log.d("Profile", "Error : ${e.message}")
         }
 
         val logoutButton = root.findViewById<Button>(R.id.logout)
 
         logoutButton.setOnClickListener{
+            Log.d("Logout", "Logout button clicked")
             apiService.logout("Bearer ${sessionManager.fetchAuthToken()}")
                 .enqueue(object : Callback<LogoutResponse> {
                     override fun onResponse(call: Call<LogoutResponse>, response: Response<LogoutResponse>) {
                         val logoutResponse = response.body()
                         Log.d("Logout", logoutResponse.toString())
-                        if (logoutResponse?.status == true) {
-                            sessionManager.setLogin(false)
-                            // Redirection vers la page de login
-                            val intent = Intent(context, LoginActivity::class.java)
-                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                            startActivity(intent)
-                            Toast.makeText(context, "Vous avez bien été déconnecté", Toast.LENGTH_SHORT).show()
-                        } else {
-                            Toast.makeText(context, "Une erreur est survenue", Toast.LENGTH_SHORT).show()
-                            Log.d("LogoutActivity", "Error during logout: ${logoutResponse?.message}")
-                        }
+                        sessionManager.setLogin(false)
+                        // Redirection vers la page de login
+                        val intent = Intent(context, LoginActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
                     }
                     override fun onFailure(call: Call<LogoutResponse>, t: Throwable) {
-                        Toast.makeText(context, "Une erreur est survenue", Toast.LENGTH_SHORT).show()
-                        Log.d("LogoutActivity", "Error during logout: ${t.message}")
+                        Log.d("Logout", "Error during logout request: ${t.message}")
+                        sessionManager.setLogin(false)
+                        // Redirection vers la page de login
+                        val intent = Intent(context, LoginActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
                     }
                 })
             sessionManager.setLogin(false)
