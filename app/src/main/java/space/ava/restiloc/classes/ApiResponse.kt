7938b83d8135c373f89dba3f0c9ede2ce8f6f283
dinholu <alizeehett@hotmail.fr>
2023-04-17 15:21:55 +0200

    Clean code

--- app/src/main/java/space/ava/restiloc/classes/ApiResponse.kt
+++ app/src/main/java/space/ava/restiloc/classes/ApiResponse.kt
@@ -2,7 +2,7 @@ package space.ava.restiloc.classes
 
 import com.google.gson.annotations.SerializedName
 
-data class LogoutResponse(
+data class ApiResponse(
 
     @SerializedName("status")
     var status: Boolean,
