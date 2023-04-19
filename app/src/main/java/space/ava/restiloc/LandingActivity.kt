fda7bb53fd01d555900d32950e571f6029b2b7e8
dinholu <alizeehett@hotmail.fr>
2023-04-19 15:29:51 +0200

    clean

--- app/src/main/java/space/ava/restiloc/LandingActivity.kt
+++ app/src/main/java/space/ava/restiloc/LandingActivity.kt
@@ -1,6 +1,7 @@
 package space.ava.restiloc
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -14,11 +15,19 @@ class LandingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_landing)
 
         val loginButton = findViewById<Button>(R.id.loginButton)
-
+        if (isDarkMode()) {
+            setTheme(R.style.Theme_MyApplication_Dark)
+        } else {
+            setTheme(R.style.Theme_MyApplication)
+        }
         // Click login button et redirection vers la page de connexion
         loginButton.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
                 }
         }
+    private fun isDarkMode(): Boolean {
+        val nightModeFlags = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        return nightModeFlags == Configuration.UI_MODE_NIGHT_YES
+    }
     }
